2ef0c37797525aa2f966871699660af38b1b0653
Yaroslav Sergienko <pallada-92@ya.ru>
2017-09-18 17:41:24 +0300

    v1

--- script.py
+++ script.py
@@ -1,13 +1,15 @@
-import requests, json, os, sys, pdb, traceback, time
+import requests, json, os, sys, pdb, traceback, time, urllib.parse
 from bs4 import BeautifulSoup
 
 db_file = 'db.txt'
+websites_db_dir = 'websites'
 
 def prog(txt):
     print(txt, end='...')
     sys.stdout.flush()
 
 def check_db(urls):
+    prog('Checking db')
     if type(urls) != dict:
         raise Exception()
     deleted = {}
@@ -16,6 +18,7 @@ def check_db(urls):
         if url in urls:
             deleted[url] = urls[url]
             del urls[url]
+    prog('%s / %s new' % (len(urls), len(urls) + len(deleted)))
     return deleted
 
 def add_to_db(urls):
@@ -24,22 +27,34 @@ def add_to_db(urls):
             f.write(url + '\n')
 
 def send(txt):
+    global last_message
+    last_message = cur_time
     if config['debug']:
         print('-----------')
         print(txt)
         print('-----------')
         return
+    prog('send len = %d' % len(txt))
     token = config['telegram_token']
     chat_id = config['telegram_chat_id']
     url = 'https://api.telegram.org/bot' + token + '/sendMessage'
-    requests.post(url, params={
+    resp = requests.post(url, params={
         'chat_id': chat_id,
         'text': txt,
     })
+    if resp.status_code != 200:
+        admin_action('TELEGRAM ERROR %s' % resp.txt)
 
 def admin_action(txt):
     send('ADMIN ACTION REQUIRED: %s' % txt)
     pdb.set_trace()
+
+def req_text(req, url):
+    if req.status_code != 200:
+        send('STATUS CODE %d for url=%s' % (req.status_code, url))
+        return None
+    txt = req.content.decode('utf-8')
+    return txt
 
 br = ' '
 replacements = [
@@ -51,6 +66,7 @@ replacements = [
     ('&#x2F;', '/'),
     ('&#39;' , '\''),
     ('&#039;', '\''),
+    ('&bull;', '•'),
     ('&#47;' , '/'),
     ('&lt;'  , '<'),
     ('&gt;'  , '>'),
@@ -61,7 +77,7 @@ replacements = [
     ('<br />', br),
     ('\n'    , br),
 ]
-    
+
 def prettify(txt):
     for replace_what, replace_with in replacements:
         txt = txt.replace(replace_what, replace_with)
@@ -101,18 +117,18 @@ def parse_upwork_json(url):
             'url': job_url,
             'time': job['publishedOn'],
         }
-    existing = check_db(jobs)
+        existing = check_db(jobs)
     if len(existing) == 1 and url in existing:
         admin_action('len(existing) == 1, url = %s' % url)
     for job in sorted(jobs.values(), key=lambda x: x['time']):
         send_job(job)
-    add_to_db(jobs)
-    pdb.set_trace()
+        add_to_db(jobs)
+        pdb.set_trace()
 
 def parse_upwork_rss(url, title):
     source = 'upwork_rss:%s' % title
     prog('Requesting %s' % (source,))
-    txt = requests.get(url).text
+    txt = req_text(requests.get(url), source)
     prog('Parsing')
     bs = BeautifulSoup(txt, 'xml')
     jobs = {url: {
@@ -134,18 +150,153 @@ def parse_upwork_rss(url, title):
             'url': item.link.text,
             'pos': len(items) - pos,
         }
-    prog('Checking db')
     existing = check_db(jobs)
-    print('%s / %s new' % (len(jobs), len(jobs) + len(existing)), end='...')
     if len(existing) == 1 and url in existing:
         admin_action('len(existing) == 1, source = %s, url = %s' % (source, url))
-    prog('Sending messages')
+        prog('Sending messages')
     for job in sorted(jobs.values(), key=lambda x: x['pos']):
         send_job(job)
     prog('Adding to db')
     add_to_db(jobs)
     print('done')
-    
+
+def extract(text, start_marker, end_marker):
+    res = []
+    idx = 0
+    while True:
+        idx_start = text.find(start_marker, idx)
+        if idx_start == -1:
+            return res
+        idx_start += len(start_marker)
+        idx_end = text.find(end_marker, idx_start)
+        if idx_end == -1:
+            return res
+        idx = idx_end + len(end_marker)
+        res.append(text[idx_start:idx_end])
+
+def get_fl_ru_token(html):
+    return extract(html, "var _TOKEN_KEY = '", "'")[0]
+
+def check_fl_ru(keywords):
+    for keyword in keywords:
+        prog('Checking fl_ru "%s" keyword' % keyword)
+        prog('Getting fl_ru token')
+        s = requests.Session()
+        url = 'https://www.fl.ru/projects/'
+        text = req_text(s.get(url), 'GET: %s' % url)
+        if text is None:
+            return
+        token = get_fl_ru_token(text)
+        prog('token=%s' % token)
+        data = (
+                'action=postfilter&kind=5&pf_category=&pf_subcategory=&'
+                'comboe_columns%5B1%5D=0&comboe_columns%5B0%5D=0&comboe_column_id=0&'
+                'comboe_db_id=0&comboe=%D0%92%D1%81%D0%B5+%D1%81%D0%BF%D0%B5%D1%86%D0%B8%D0%B0%D0%BB%D0%B8%D0%B7%D0%B0%D1%86%D0%B8%D0%B8&'
+                'location_columns%5B1%5D=0&location_columns%5B0%5D=0&location_column_id=0&location_db_id=0&'
+                'location=%D0%92%D1%81%D0%B5+%D1%81%D1%82%D1%80%D0%B0%D0%BD%D1%8B&pf_cost_from=&currency_text_columns%5B1%5D=0&'
+                'currency_text_columns%5B0%5D=2&currency_text_column_id=0&currency_text_db_id=2&pf_currency=2&'
+                'currency_text=%D0%A0%D1%83%D0%B1&'
+            ) + 'pf_keywords=%s&u_token_key=%s' % (urllib.parse.quote(keyword), urllib.parse.quote(token))
+        prog('Making POST request')
+        text = req_text(s.post(
+            url,
+            data=data,
+            headers={'Content-Type': 'application/x-www-form-urlencoded'},
+        ), 'POST: fl.ru %s' % keyword)
+        # token = get_fl_ru_token(text)
+        # prog('token=%s' % token)
+        # pdb.set_trace()
+        projects = extract(text, 'id="project-item', '"')
+        source = 'fl.ru: %s' % keyword
+        jobs = {source: {
+            'url': source,
+            'pos': -100,
+        }}
+        for pos, project in enumerate(projects):
+            url = 'https://www.fl.ru/projects/%s' % project
+            jobs[url] = {
+                'url': url,
+                'pos': -pos,
+            }
+        existing = check_db(jobs)
+        if len(existing) == 1 and url in existing:
+            admin_action('len(existing) == 1, source = %s, url = %s' % (source, url))
+        if len(jobs) > 0:
+            prog('Sending messages')
+            for job in sorted(jobs.values(), key=lambda x: x['pos']):
+                send('fl.ru: %s' % job['url'])
+        prog('Adding to db')
+        add_to_db(jobs)
+        print('done')
+        
+
+def get_strings(html, select=None):
+    bs = BeautifulSoup(html, 'lxml')
+    if select is not None:
+        bs = bs.select(select)[0]
+    for s in bs(['script', 'style']):
+        s.extract()
+    txt = bs.getText(separator=' ')
+    res = []
+    for line in txt.split('\n'):
+        pret = prettify(line)
+        if pret != '':
+            res.append(pret)
+    return res
+
+url_replace = [
+    ('/', '_'),
+    ('.', '_'),
+    ('?', '_'),
+    ('=', '_'),
+    ('&', '_'),
+]
+
+def get_website_file(url):
+    if url.startswith('http://'):
+        url = url[len('http://'):]
+    if url.startswith('https://'):
+        url = url[len('https://'):]
+    if url.startswith('www.'):
+        url = url[len('www.'):]
+    if url.endswith('/'):
+        url = url[:-1]
+    for repl_what, repl_with in url_replace:
+        url = url.replace(repl_what, repl_with)
+    return os.path.join(websites_db_dir, url)
+
+def check_website(task):
+    if type(task) == str:
+        task = {'url': task}
+    if 'select' not in task:
+        task['select'] = None
+    url = task['url']
+    prog('Requesting %s' % url)
+    text = req_text(requests.get(url), url)
+    txt = get_strings(text, select=task['select'])
+    fname = get_website_file(url)
+    if os.path.exists(fname):
+        prev_lines = set()
+        for line in open(fname):
+            line = line.strip()
+            prev_lines.add(line)
+    else:
+        prog('First version of %s' % fname)
+        prev_lines = []
+    new_txt = []
+    for line in txt:
+        if line not in prev_lines:
+            new_txt.append(line)
+    if len(new_txt) == 0:
+        print('No changes. done.')
+        return
+    prog('%d new lines' % len(new_txt))
+    send('%s ::: %s' % (url, ' / '.join(new_txt)))
+    prog('Writing new strings')
+    with open(fname, 'w') as f:
+        f.write('\n'.join(txt))
+    # pdb.set_trace()
+    print('done')
 
 if not os.path.exists('config.json'):
     print('Please, create config.json file with "telegram_token" and "telegram_chat_id" fields.')
@@ -153,15 +304,40 @@ if not os.path.exists('config.json'):
 if not os.path.exists(db_file):
     print('Creating %s file.' % db_file)
     open(db_file, 'w')
+if not os.path.exists(websites_db_dir):
+    print('Creating %s dir.' % websites_db_dir)
+    os.makedirs(websites_db_dir)
 config = json.load(open('config.json'))
 
+last_message = 0
+last_upwork_req = 0
+last_websites_req = 0
+last_fl_ru_req = 0
 try:
     while True:
-        for title, url in config['upwork_rss']:
-            parse_upwork_rss(url, title)
-        print('Sleep for %d minutes' % config['sleep_minutes'])
-        time.sleep(config['sleep_minutes'] * 60)
+        cur_time = time.time() / 60
+        if cur_time - last_upwork_req >= config['upwork_sleep']:
+            for title, url in config['upwork_rss']:
+                if 1:
+                    parse_upwork_rss(url, title)
+            last_upwork_req = cur_time
+        if cur_time - last_websites_req >= config['websites_sleep']:
+            for url in config['websites']:
+                if 1:
+                    check_website(url)
+            last_websites_req = cur_time
+        if cur_time - last_fl_ru_req >= config["fl_ru_sleep"]:
+            if 1:
+                check_fl_ru(config['fl_ru_keywords'])
+            last_fl_ru_req = cur_time
+        if cur_time - last_message >= config['ping_period']:
+            send('OK')
+        print(end=',')
+        sys.stdout.flush()
+        time.sleep(60)
 except Exception as e:
+    if type(e).__name__ == 'BdbQuit':
+        sys.exit()
     exc = traceback.format_exc()
     print(exc)
     send(exc)
---
+++ websites/habidatum.com
@@ -0,0 +1,147 @@
+Habidatum
+Platform
+Applications
+Projects
+About
+Habidatum Platform
+Habidatum’s services are built around a powerful urban data platform capable of collecting, storing, processing and visualizing quantitative and qualitative urban data.
+This platform makes an interaction with real-time data flows through the customized visualization interface possible.
+Machine-learning technologies and the system of automated measurements make complex analytical support quick and easy.
+The platform is created and supported by a group of highly professional and experienced urban specialists working globally for Habidatum.
+Data analysis workflow
+We assemble and fine-tune software, train algorithms, develop new visualization techniques and build analytical methodologies for processing of raw data into the deliverables for decision-makers: reports and dashboards, monitoring and alert systems, interactive analytical visualization interfaces with data sample export and customized report building functions.
+Key advantages
+Platform architecture
+Ensuring flexibility
+— Module-based structure
+— Various deployment options
+— Customized formats of deliverables
+Chronotope
+Dealing with space-time patterns
+— Unique visualization technique
+— Granular, but non-privacy-intrusive
+— Multiple scales, angles and dimensions
+— Brand new measurements for further use by analysts
+and decision-makers
+Palimpsest
+Colliding layers of data
+— Data integration requirements: time- and geotags available or possible to generate
+— Underlying data science: machine-learning and human training
+— Balancing temporal and spatial scales in real time
+— Language agnostic text analysis: sentiment analysis,
+entity extraction, topic analysis, clustering
+Dialogue
+Interacting with data through analytical visualization
+— Analytical visualization: intuitive visuals for analysis
+— Visualization for considerable reduction in decision-making time
+— Interactive visualization can be customized by the user
+— Filtering: focus on target groups and locations
+Applications
+Professional interfaces transforming flows of spontaneous data into the ready-to-use system of indicators and visual patterns.
+Alternative statistics
+Benchmarking: consistent vs. deviant
+Transportation system and road network optimization
+Increasing neighborhood / customer satisfaction
+Target audience for urban projects
+Evaluation of the commercial power of a place
+Time coordination
+Measuring
+Monitoring
+Planning
+Mobility
+Activity
+Discussions
+Alternative statistics
+Indicators tailored from spontaneous data flows (social media posts, cell phone signals, sensor information) substituting for regular statistics.
+Identifying manifestations of urban life in space and time (vernacular regions, daily routines, lifestyles and choices).
+Benchmarking: consistent vs. deviant
+Real-time monitoring, impact analysis, risk assessment.
+Transportation system and road network optimization
+Measuring connectivity and accessibility; identifying network deficiencies; estimating economic value of urban infrastructure projects.
+Increasing neighborhood / customer satisfaction
+Sentiment / behavioral analysis associated with various groups of population, topics and localities; neighborhood satisfaction indexes.
+Target audience for urban projects
+Identifying and monitoring behavioral patterns affecting the efficiency and effectiveness of urban projects.
+Evaluation of the commercial power of a place
+Crossing behavioral patterns with flows and transactions.
+Time coordination
+Time-sharing solutions for congested and over-priced areas.
+Rio Semantic Landscapes — Beyond Control Syntax?
+2017
+Rio de Janeiro
+social media, open data
+Value of Satisfaction: London
+2016
+London
+Social media, open data
+Easy Access: Russian Cities
+2016
+Kazan, Ufa, Nizhny Novgorod, Yaroslavl
+Open data
+Value-Driven Values
+2016
+Kazan, Ufa, Nizhny Novgorod, Yaroslavl, Khabarovsk
+Open data, Instagram, AIRSTATE, real estate prices
+Cities without Boundaries
+2016
+Kazan, Ufa, Nizhny Novgorod, Yaroslavl, Khabarovsk
+Open data, social media
+Easy Access: Pune
+2016
+Pune
+Open data
+Les Liaisons Miracles: Coral Gables
+2015
+Coral Gables
+Social media
+Dubai Valves of Mobility
+2015
+Dubai
+Cell phone
+DWTC Semantic Links to Dubai
+2015
+Dubai
+Social media
+Chronotope City: New York
+2015
+New York City
+Open data
+DPAC: Beats of Culture
+2015
+Denver
+Social media
+Barcelona vs. #Barcelona
+2015
+Barcelona
+Social media
+G2C Cooperation in Cities Globally
+2014
+International
+Reports & interviews
+Chronotope City: Moscow
+2014
+Moscow
+Open data
+Emotions: Muscovites’ Mental Geography
+2013
+Moscow
+Social media
+Motions: Muscovites’ Mobility Patterns
+2013
+Moscow
+Cell phone
+Sort by: Year Data City
+Platform
+Applications
+Projects
+About
+Vision
+Team
+Contacts
+© 2015 Habidatum
+Made by Binary Cat
+Urban data platform helping professionals to analyze,
+measure and manage dynamic urban communities.
+Platform
+Applications
+Projects
